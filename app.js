4df6f328bdceec679599a548fc4febee9b4cf6ca
unknown <omkar.patil@graymatrix.com>
2020-08-19 10:13:40 +0530

    populating entries error

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
-
-
-
-var Sequelize = require('sequelize');
+// 
+// const config = require('./config/config.json')[process.env.NODE_ENV];
+const config = require('./config/config.json')["development"];
+var Sequelize = require('sequelize'); console.log(config);	
 
 // creating connection with mysql 
-var connetion = new Sequelize('demo_schema','root','1234');
+var connetion = new Sequelize('demo_schema','root','1234',{...config});
 
 // to define a model in sequelize, .define() method
 var Article = connetion.define('article',{  // makes article the name of the model
@@ -17,8 +16,28 @@ var Article = connetion.define('article',{  // makes article the name of the mod
 }
 });
 
+
+
 // this creates a database if there is nothing created 
-connetion.sync({
-	force: true,
-	logging: console.log
-}); 
+// connetion.sync({
+// 	force: true,
+// 	logging: console.log
+// }); 
+
+
+// this will populate the table article with given data
+connetion.sync().then(function () {
+	Article.create({
+		title: 'some title',
+		body: 'some long body for some title'
+
+	});
+});
+
+
+// to find an entry with id
+// connetion.sync().then(function () {
+// 	Article.findById(1).then(function(article){
+// 		console.log(article.dataValues)
+// 	})
+// })
